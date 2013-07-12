d8362e2f6d763981fd32ffafe00af21db1a06cdf
Kenneth Ng <ngkenneth@gmail.com>
2013-07-12 02:28:45 -0700

    new index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 22!');
+  var fileString = fs.readFileSync('index.html', 'utf8');
+  response.send(fileString);
 });
 
 var port = process.env.PORT || 5000;
